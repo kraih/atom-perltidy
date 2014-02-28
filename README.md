9dc0ebe5bfe500dd9912ded6f2264f963e8bf93d
Sebastian Riedel <sri@cpan.org>
2014-02-28 04:43:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Perltidy
+
+Perltidy for Atom.
+
+Start with `alt-p`.
+
+## Copyright And License
+
+  Copyright (C) 2014, Sebastian Riedel.
+
+  This program is free software, you can redistribute it and/or modify it
+  under the terms of the Artistic License version 2.0.
---
+++ index.js
@@ -0,0 +1,42 @@
+var fs    = require('fs'),
+    spawn = require('child_process').spawn;
+
+module.exports = {
+
+  configDefaults: {
+    binary: "/usr/local/bin/perltidy"
+  },
+
+  activate: function() {
+    atom.workspaceView.command('perltidy:tidy', function() {
+
+      var editor = atom.workspaceView.getActiveView().editor;
+      var path   = atom.config.get('perltidy.binary');
+
+      if (fs.existsSync(path)) {
+        perlTidy(path, editor.getText(), function (perl) {
+          editor.setText(perl);
+        });
+      }
+
+      else {
+        console.error('No Perltidy found at "' + path + '".');
+      }
+    });
+  }
+};
+
+function perlTidy(path, before, cb) {
+
+  var after = '';
+  var perltidy = spawn(path);
+  perltidy.stdin.setEncoding  = 'utf-8';
+  perltidy.stdout.setEncoding = 'utf-8';
+  perltidy.stdin.end(before);
+  perltidy.on('exit', function() {
+    cb(after);
+  });
+  perltidy.stdout.on('data', function(chunk) {
+    after += chunk;
+  });
+}
